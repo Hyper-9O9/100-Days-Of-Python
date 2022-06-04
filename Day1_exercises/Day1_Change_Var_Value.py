689da3d3f3d1bd9033be53542ed101486382e14c
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-06-04 15:28:26 +0300

    Add files via upload

---
+++ Day1_exercises/Day1_Change_Var_Value.py
@@ -0,0 +1,11 @@
+a = input("a:")
+b = input("b:")
+
+tmp = a
+
+a = b
+b = tmp
+
+
+print("a = "+a)
+print("b = "+b)
---
+++ Day1_exercises/Day1_Name_Length.py
@@ -0,0 +1,2 @@
+print("Find lenght of a name")
+print(len(input("What is your name? ")))
