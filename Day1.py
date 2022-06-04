b4e1afe0c122fc8a10e514535002ac7e365324e4
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-06-04 13:09:36 +0300

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,5 @@
+#a simple name generator excersise
+print("Welcome to the Band Name Generator.")
+city_Input = input("Whats's name of the city you grew up in?\n") #input() used for taking string as an input from user
+animal_Input = input("Whats's your pet's name?\n")
+print("Your band name could be "+city_Input+" "+animal_Input)
