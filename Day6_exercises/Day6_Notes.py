7c4f0cf2ed6de1a465d63f23fa4a796430e2c957
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2022-06-11 13:09:09 +0300

    Add files via upload

---
+++ Day6_exercises/Day6_Notes.py
@@ -0,0 +1,28 @@
+# to define a function
+# we use keyword "def" followed by our function name
+def my_function():
+    print("content of the function")
+# after creating the function, to use it we call at next line without space
+my_function()
+
+#practive site
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=%2Fworlds%2Fmenus%2Fselect_collection_en.json&name=a&url=user_world%3Aa
+
+# #def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def sequence():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+# while at_goal() == False:
+#     sequence()
+#
+# print("Goal Reached")
