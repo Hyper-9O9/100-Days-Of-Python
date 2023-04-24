425fd96c9aa3fc57c6d57e1c7737743dd203f482
Hersh Hiwa <85962934+hollow9o9@users.noreply.github.com>
2023-04-24 11:44:33 +0300

    Add files via upload

--- DAY20_exercises/CREATE_SNAKE.py
+++ DAY20_exercises/CREATE_SNAKE.py
@@ -1,1 +1,24 @@
 # creating the snake game using python
+# the aim is just to create the snake on the screen
+from turtle import Screen, Turtle
+# setting up the game
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+# creating the postion for the snake
+start_x = 0
+start_y = 0
+reducer = 0
+snake = []
+# setting the snake on the map
+for initialize in range(3):
+    turtle = Turtle(shape="square")
+    turtle.color("white")
+    turtle.penup()
+    turtle.goto(start_x - reducer, start_y)
+    turtle.pendown()
+    reducer = reducer + 20
+    snake.append(turtle)
+
+screen.exitonclick()
